1123129191548a85449d41f1c2ed9d4c4d2fa504
Nathan Totten <ntotten@auth0.com>
2015-07-31 12:27:59 -0700

    Improved content tests perf. Removed broken link test

--- test/packager.test.js
+++ test/packager.test.js
@@ -6,7 +6,8 @@ var urlJoin = require('url-join');
 
 var baseUrl = urlJoin('http://localhost:' + nconf.get('PORT'), nconf.get('BASE_URL'));
 
-describe('Application', function() {
+
+describe('Packager', function() {
   after(function(done) {
     nconf.set('ENABLE_PACKAGE_TEST', false);
     docsapp.stop(done);
@@ -16,16 +17,14 @@ describe('Application', function() {
     docsapp.start(done);
   });
 
-  describe('Packager', function() {
-    it('should download seed project', function(done) {
-      this.timeout(30000);
-      var packageTestUrl = urlJoin(baseUrl, '/native-mobile-samples/master/create-package?path=Android/basic-sample&type=replace&filePath=Android/basic-sample/app/src/main/res/values/auth0.xml');
-      request(packageTestUrl,
-        function(error, response, body) {
-          assert.notEqual(body.length, 0);
-          assert.equal(response.statusCode, 200);
-          done();
-        });
-    });
+  it('should download seed project', function(done) {
+    this.timeout(30000);
+    var packageTestUrl = urlJoin(baseUrl, '/native-mobile-samples/master/create-package?path=Android/basic-sample&type=replace&filePath=Android/basic-sample/app/src/main/res/values/auth0.xml');
+    request(packageTestUrl,
+      function(error, response, body) {
+        assert.notEqual(body.length, 0);
+        assert.equal(response.statusCode, 200);
+        done();
+      });
   });
 });
