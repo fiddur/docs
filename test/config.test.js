db8c55b1db699388957e6f4e3cf39e1fc92ab5bb
Nathan Totten <ntotten@auth0.com>
2015-08-19 11:18:54 -0700

    Added test to validate how-to configuration

---
+++ test/config.test.js
@@ -0,0 +1,30 @@
+var nconf = require('nconf');
+var path = require('path');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var _ = require('lodash');
+var assert = require('assert');
+
+var docsPath = nconf.get('DOCS_PATH') || path.resolve(__dirname, '../docs/articles');
+
+var settings = yaml.safeLoad(fs.readFileSync(path.join(docsPath, '../config/settings.yml')));
+var menus = yaml.safeLoad(fs.readFileSync(path.join(docsPath, '../config/menus.yml')));
+var navigation = yaml.safeLoad(fs.readFileSync(path.join(docsPath, '../config/navigation.yml')));
+
+describe('Configuration', function() {
+  describe('Navigation.yml', function() {
+    it.only('Has valid attributes for How To', function() {
+      var category = _.find(navigation.categories, { id: 'how-to'});
+      assert.notEqual(category.name.length, 0, 'Invalid category name.');
+      assert.notEqual(category.description.length, 0, 'Invalid category description');
+      assert.ok(category.links.length > 9, 'Not enough how-tos specified.');
+      for (var i = 0; i < category.links.length; i++) {
+        var link = category.links[i];
+        assert.notEqual(link.name.length, 0, 'Invalid link name.');
+        assert.notEqual(link.icon.length, 0, 'Invalid link icon.');
+        assert.notEqual(link.href.length, 0, 'Invalid link href.');
+        assert.notEqual(link.description.length, 0, 'Invalid link description.');
+      }
+    });
+  });
+});
