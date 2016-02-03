9e0d6ad2c2c67f2f9b8491a5df3a3872a41dbed9
Nathan Totten <ntotten@auth0.com>
2016-02-03 14:42:35 -0800

    Disabled frameblock

--- server.js
+++ server.js
@@ -97,13 +97,7 @@ server.use(bodyParser.urlencoded({ extended: false }));
 
 
 // security headers
-var ALLOWED_ORIGINS = nconf.get('ALLOWED_ORIGINS');
-if (ALLOWED_ORIGINS) {
-  server.use(helmet.frameguard('allow-from', ALLOWED_ORIGINS));
-} else {
-  server.use(helmet.frameguard('sameorigin'));
-}
-
+//server.use(helmet.frameguard());
 server.use(helmet.hsts( { maxAge: 31536000000 } ));
 server.use(helmet.xssFilter());
 server.use(helmet.noSniff());
