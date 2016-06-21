7ef7ba9fe440bc47ed471de3d9dfd99b6b57058d
Nathan Totten <ntotten@auth0.com>
2016-06-21 15:46:06 -0700

    Server side anchor links

--- client/browser/anchorLinks.js
+++
@@ -1,9 +0,0 @@
-/* global $ */
-
-export default function setAnchorLinks() {
-  $('body').on('click', '.docs-content .anchor-heading', function() {
-    if($(this).attr('id') && !$(this).is('h1')) {
-      window.location.hash = $(this).attr('id');
-    }
-  })
-}
