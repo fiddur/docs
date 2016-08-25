989eeb7a3990f027244e9881f86db42d72b25909
Nathan Totten <nathan@ntotten.com>
2016-08-24 17:08:12 -0700

    Revert "Implement new header"

--- client/browser/header.js
+++
@@ -1,7 +0,0 @@
-import React, { createElement } from 'react';
-import { render } from 'react-dom';
-import Header from '../../universal/components/Header';
-
-const renderHeader = () => render(<Header />, document.getElementById('header'));
-
-export default renderHeader;
