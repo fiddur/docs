70695c86b4fe2c21153f8ed6d7e34b7a1d484ca0
Nathan Totten <ntotten@auth0.com>
2016-09-06 11:25:24 -0700

    Removed old search implimentation

--- config.js
+++ config.js
@@ -49,7 +49,6 @@ nconf.file('global', { file: config_file })
     'CONSOLE_LOG_LEVEL': 'debug',
     'LOG_TO_KINESIS_LEVEL': 'info',
     'LOG_TO_WEB_LEVEL':     'error',
-    'SEARCH_ENGINE_ID': '010795999221123445302:urkvlewda8q', // Google Site Search ID
     'PINGDOM_ID': process.env.NODE_ENV === 'production' ? '565cb401abe53d7b2cda7732' : false,
     'SENTRY_DSN': process.env.NODE_ENV === 'production' ? 'https://29ef6b5c3bbd4b9ba9f5710014e20c03@app.getsentry.com/57750' : false,
     'MOUSEFLOW_ID': process.env.NODE_ENV === 'production' ? '1f318101-e4cd-4d84-b95e-6a0e6771798d' : false,
