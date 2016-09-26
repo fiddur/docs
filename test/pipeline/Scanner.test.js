f310c5381575c446840467f8046476d4389759bc
Nate Kohari <nkohari@gmail.com>
2016-09-26 11:25:45 -0400

    First cut of document rendering pipeline

---
+++ test/pipeline/Scanner.test.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import Scanner from '../../lib/pipeline/Scanner';
+
+describe('Scanner', () => {
+
+  it('can be created', () => {
+    let scanner = new Scanner();
+    
+  });
+
+});
