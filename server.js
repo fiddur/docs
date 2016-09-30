21c378f8e28239512a4c3bc5aded308d075de2d6
Nathan Totten <ntotten@auth0.com>
2016-09-30 14:31:37 -0700

    Middleware cleanup

--- server.js
+++ server.js
@@ -15,8 +15,21 @@ import agent from './lib/logs';
 import eventLogger from './lib/logs/event-logger';
 import requestLogger from './lib/logs/request-logger';
 import bootstrap from './lib/bootstrap';
+import cors from './lib/middleware/cors';
+import defaultValues from './lib/middleware/default-values';
+import embedded from './lib/middleware/embedded';
+import overrideIfAuthenticated from './lib/middleware/override-if-authenticated';
+import overrideClientQsPublic from './lib/middleware/override-client-qs-public-urls';
+import overrideClientQs from './lib/middleware/override-client-qs';
+import setCurrentTenant from './lib/middleware/set-current-tenant';
+import setUserIsOwner from './lib/middleware/set-user-is-owner';
+import urlVariables from './lib/middleware/url-variables';
+import fetchABExperiments from './lib/middleware/ab-testing';
+import csp from './lib/middleware/csp';
+import redirectQuickstarts from './lib/middleware/redirect-quickstarts';
 import assetBundles from './lib/middleware/asset-bundles';
 import htmlComponents from './lib/middleware/html-components';
+import sessionStore from './lib/session-store';
 
 const logger = agent.logger;
 
@@ -27,44 +40,46 @@ eventLogger.watch(process);
 server.use(agent.errorReporter.express.requestHandler);
 server.use(agent.errorReporter.express.errorHandler);
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   if (!nconf.get('db')) {
     return done(null, user);
   }
-  done(null, user.id);
+  return done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser((id, done) => {
   if (!nconf.get('db')) {
-    return done(null, id);
-  }
+    done(null, id);
+  } else {
+    const getDb = require('./lib/data');
 
-  var getDb = require('./lib/data');
-  getDb(function(db){
-    var userColl = db.collection('tenantUsers');
-    userColl.findOne({id: id}, done);
-  });
+    getDb((db) => {
+      const userColl = db.collection('tenantUsers');
+      userColl.findOne({ id }, done);
+    });
+  }
 });
 
 server.set('view engine', 'jade');
 server.enable('trust proxy');
 
 if (nconf.get('NODE_ENV') === 'production') {
-  server.use(function(req, res, next){
+  server.use((req, res, next) => {
     if (nconf.get('dontForceHttps') || req.originalUrl === '/test') return next();
 
-    if(req.headers['x-forwarded-proto'] !== 'https')
+    if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(nconf.get('DOMAIN_URL_DOCS') + req.url);
+    }
 
-    next();
+    return next();
   });
 }
 
-server.use('/test', function (req, res) {
+server.use('/test', (req, res) => {
   res.sendStatus(200);
 });
 
-server.use('/docs/test', function (req, res) {
+server.use('/docs/test', (req, res) => {
   res.sendStatus(200);
 });
 
@@ -72,11 +87,9 @@ if (nconf.get('NODE_ENV') !== 'test') {
   server.use(requestLogger);
 }
 
-var middleware = require('./lib/middleware');
-var sessionStore = require('./lib/session-store');
-
-
 server.use(cookieParser());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(session({
   secret: nconf.get('sessionSecret'),
   store: sessionStore ? sessionStore(session) : undefined,
@@ -84,31 +97,18 @@ server.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    domain:   nconf.get('COOKIE_SCOPE'),
-    path:     '/',
+    domain: nconf.get('COOKIE_SCOPE'),
+    path: '/',
     httpOnly: true,
-    maxAge:   null,
-    secure:   !nconf.get('dontForceHttps') && nconf.get('NODE_ENV') === 'production'
+    maxAge: null,
+    secure: !nconf.get('dontForceHttps') && nconf.get('NODE_ENV') === 'production'
   }
 }));
 
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
-
-
 // security headers
-server.use(middleware.csp);
-server.use(middleware.cors);
-server.use(function(req, res, next) {
-  if (req.path.indexOf('/docs/lock-demos/') === 0) {
-    // Lock demos are used on manage.auth0.com and X-Frame-Options
-    // doesn't support multiple values so we allow all hosts for this path
-    next();
-  } else {
-    // For all other paths, we deny
-    helmet.frameguard({ action: 'sameorigin' })(req, res, next);
-  }
-});
+server.use(csp);
+server.use(cors);
+server.use(helmet.frameguard({ action: 'sameorigin' }));
 server.use(helmet.hsts({ maxAge: 31536000000 }));
 server.use(helmet.xssFilter());
 server.use(helmet.noSniff());
@@ -117,23 +117,23 @@ server.use(helmet.hidePoweredBy());
 server.use(methodOverride());
 
 server.use('/docs/media', express.static(path.join(__dirname, 'docs/media')));
-['css', 'img', 'js', 'vendor'].forEach(function(folder) {
-  server.use('/docs/' + folder, express.static(path.join(__dirname, '/public/', folder)));
+['css', 'img', 'js', 'vendor'].forEach((folder) => {
+  server.use(`/docs/${folder}`, express.static(path.join(__dirname, '/public/', folder)));
 });
 
 
 server.use(passport.initialize());
 server.use(passport.session());
-server.use(middleware.setCurrentTenant);
-server.use(middleware.setUserIsOwner);
-server.use(middleware.defaultValues);
-server.use(middleware.embedded);
-server.use(middleware.overrideIfAuthenticated);
-server.use(middleware.overrideIfClientInQs);
-server.use(middleware.overrideIfClientInQsForPublicAllowedUrls);
-server.use(middleware.urlVariables);
-server.use(middleware.fetchABExperiments);
-server.use(middleware.redirectQuickstarts);
+server.use(setCurrentTenant);
+server.use(setUserIsOwner);
+server.use(defaultValues);
+server.use(embedded);
+server.use(overrideIfAuthenticated);
+server.use(overrideClientQs);
+server.use(overrideClientQsPublic);
+server.use(urlVariables);
+server.use(fetchABExperiments);
+server.use(redirectQuickstarts);
 server.use(assetBundles);
 server.use(htmlComponents);
 
@@ -144,50 +144,38 @@ server.use('/docs', require('./lib/feedback'));
 server.use('/docs', require('./lib/sitemap'));
 server.use('/docs', require('./lib/updates'));
 server.use('/docs', require('./lib/redirects'));
-
-
-server.get('/docs/switch', function (req, res) {
-  req.session.current_tenant = {
-    name: req.query.tenant,
-    region: req.query.region,
-  };
-  res.redirect('/docs');
-});
-
 server.use('/docs/meta', require('./lib/api'));
 server.use('/docs', require('./lib/app'));
 
 // This is just for localhost
-server.get('/', function(req, res) {
+server.get('/', (req, res) => {
   res.redirect('/docs');
 });
 
 // catch 404 and forward to error handler
-server.use(function(req, res, next) {
-  var err = new Error('Not Found');
+server.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-server.use(function(internalErr, req, res, next) {
-
+server.use((internalErr, req, res, next) => {
   // Log server errors.
   if (internalErr.status > 499) {
     logger.error(internalErr);
   }
 
   // Create a santitized, serializable error that we can return to the client.
-  let err = {
+  const err = {
     status: internalErr.status || 500,
-    title:  internalErr.status === 404 ? strings.PAGE_NOT_FOUND : strings.ERROR_PROCESSING_REQUEST
+    title: internalErr.status === 404 ? strings.PAGE_NOT_FOUND : strings.ERROR_PROCESSING_REQUEST
   };
 
   // Don't send back stack traces in production.
   if (process.env.NODE_ENV === 'production') {
     err.message = err.title;
     err.stack = '';
-  }
-  else {
+  } else {
     err.message = internalErr.message;
     err.stack = internalErr.stack;
   }
@@ -197,26 +185,21 @@ server.use(function(internalErr, req, res, next) {
     .status(err.status)
     .send(`<h1>${err.title}</h1><h2>${err.status}</h2><pre>${err.stack}</pre>`)
     .end();
+  } else {
+    bootstrap({ err }, req, res, next);
   }
-  else {
-    bootstrap({err}, req, res, next);
-  }
-
 });
 
-server.use(function(err, req, res, next) {
-
+server.use((err, req, res, next) => {
   // This is the worst-case scenario. If we've gotten here, the err page itself
   // encountered an error during rendering. If we're in production, just write
   // out a simple message; otherwise, dump the stack trace.
   if (process.env.NODE_ENV === 'production') {
     res.status(err.status || 500);
     res.render('error');
-  }
-  else {
+  } else {
     next(err);
   }
-
 });
 
 export default server;
