40a4498355e2ddaf11c0eeacad76eae16ea9eb80
Nathan Totten <ntotten@auth0.com>
2016-09-30 09:32:50 -0700

    Removed line numbers on code blocks

--- client/browser/highlightCode.js
+++ client/browser/highlightCode.js
@@ -6,59 +6,17 @@ hljs.configure({
   classPrefix: ''
 });
 
-
-// https://github.com/wcoder/highlightjs-line-numbers.js
-function lineNumbersBlock (element) {
-  if (typeof element !== 'object') return;
-
-  var parent = element.parentNode;
-  var lines = getCountLines(parent.textContent);
-
-  if (lines > 1) {
-    var l = '';
-    for (var i = 0; i < lines; i++) {
-      l += (i + 1) + '\n';
-    }
-
-    var linesPanel = document.createElement('code');
-    linesPanel.className = 'hljs hljs-line-numbers';
-    linesPanel.style.float = 'left';
-    linesPanel.textContent = l;
-
-    parent.insertBefore(linesPanel, element);
-  }
-}
-
-function getCountLines(text) {
-  if (text.length === 0) return 0;
-
-  var regExp = /\r\n|\r|\n/g;
-  var lines = text.match(regExp);
-  lines = lines ? lines.length : 0;
-
-  if (!text[text.length - 1].match(regExp)) {
-    lines += 1;
-  }
-
-  return lines;
-}
-
-hljs.lineNumbersBlock = lineNumbersBlock;
-
 export default function highlightCode() {
-  $('pre code').each(function(i, block) {
-    var $snippet = $(this);
+  $('pre code').each((i, block) => {
+    const $snippet = $(this);
 
     // Dont do highlight on auth api snippets
     if ($snippet.parents('.api-explorer.nocode').length) {
       return;
     }
 
-    if(!$snippet.hasClass('hljs')) {
+    if (!$snippet.hasClass('hljs')) {
       hljs.highlightBlock(block);
-      if (!$snippet.hasClass('no-lines')) {
-        hljs.lineNumbersBlock(block);
-      }
       $snippet.addClass('hljs');
     }
   });
