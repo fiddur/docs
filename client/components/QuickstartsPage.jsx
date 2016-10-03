372042383ba5027e6675e83e0ee4604f4359f7e8
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-03 12:29:36 -0300

    Remove owl-carousel initialization and comment styles.

--- client/components/QuickstartsPage.jsx
+++ client/components/QuickstartsPage.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import {TutorialNavigator} from 'auth0-tutorial-navigator';
-import {quickstartNavigationAction} from '../action/quickstartNavigationAction';
+import { TutorialNavigator } from 'auth0-tutorial-navigator';
+import { quickstartNavigationAction } from '../action/quickstartNavigationAction';
 import NavigationBar from './NavigationBar';
-
-// TODO: This depends on a "carousel" ref that's set by the TutorialNavigator itself.
-// Can we move this into the component's codebase somehow?
-var initCarouselInBrowser = function() {
-  var $carousel = $(this.refs.carousel);
-  $carousel.owlCarousel({
-    margin: 20,
-    center: true,
-    dots: true,
-    navContainerClass: 'nav',
-    navClass: ['prev', 'next'],
-    baseClass: 'js-carousel',
-    itemClass: 'item',
-    dotsClass: 'dots',
-    dotClass: 'dot',
-    nav: false,
-    responsive: {
-      0:   {items: 1, stagePadding: 60},
-      380: {items: 2, stagePadding: 0},
-      570: {items: 3, stagePadding: 0, autoWidth: true, center: false},
-      768: {items: 4, stagePadding: 0, autoWidth: true, center: false},
-    }
-  });
-};
 
 class QuickstartsPage extends React.Component {
 
@@ -33,7 +9,11 @@ class QuickstartsPage extends React.Component {
     return (
       <div className="document docs-quickstart-selector">
         <NavigationBar currentSection="quickstarts" />
-        <TutorialNavigator {...this.props} customNavigationAction={quickstartNavigationAction} componentLoadedInBrowser={initCarouselInBrowser} />
+        <TutorialNavigator
+          {...this.props}
+          customNavigationAction={quickstartNavigationAction}
+          componentLoadedInBrowser={() => {}}
+        />
       </div>
     );
   }
