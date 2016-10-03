65eb2e081b4898f4dc70d739676712bff6f48b47
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-03 14:24:06 -0300

    Remove import of owl-carousel.

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -7,30 +7,6 @@ import CategoryCard from './CategoryCard';
 import TryBanner from './TryBanner';
 import SearchBox from './SearchBox';
 
-// TODO: This depends on a "carousel" ref that's set by the TutorialNavigator itself.
-// Can we move this into the component's codebase somehow?
-var initCarouselInBrowser = function() {
-  var $carousel = $(this.refs.carousel);
-  $carousel.owlCarousel({
-    margin: 20,
-    center: true,
-    dots: true,
-    navContainerClass: 'nav',
-    navClass: ['prev', 'next'],
-    baseClass: 'js-carousel',
-    itemClass: 'item',
-    dotsClass: 'dots',
-    dotClass: 'dot',
-    nav: false,
-    responsive: {
-      0:   {items: 1, stagePadding: 60},
-      380: {items: 2, stagePadding: 0},
-      570: {items: 3, stagePadding: 0, autoWidth: true, center: false},
-      768: {items: 4, stagePadding: 0, autoWidth: true, center: false},
-    }
-  });
-};
-
 class Home extends React.Component {
 
   render() {
@@ -49,7 +25,11 @@ class Home extends React.Component {
             <h1>Documentation</h1>
           </div>
         </div>
-        <TutorialNavigator {...this.props} customNavigationAction={quickstartNavigationAction} componentLoadedInBrowser={initCarouselInBrowser} />
+        <TutorialNavigator
+          {...this.props}
+          customNavigationAction={quickstartNavigationAction}
+          componentLoadedInBrowser={() => {}}
+        />
         {tryBanner}
         <div className="category-cards container center-block">
           <h1>Curated content to fully understand our platform</h1>
