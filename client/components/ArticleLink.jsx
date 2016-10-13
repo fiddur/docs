25788f7c6bd3d4db4277e8a838f14411cd840e20
Ariel Gerstein <ariel.ger@hotmail.com>
2016-10-13 15:45:39 -0300

    Use NavLink followLink prop instead of rendering a link (sidebar).

--- client/components/ArticleLink.jsx
+++ client/components/ArticleLink.jsx
@@ -5,17 +5,13 @@ class ArticleLink extends React.Component {
 
   render() {
     let { article, children } = this.props;
-    if (article.forceFullReload) {
-      return (
-        <a href={article.url} className={`${article.external ? 'arrow-item' : ''}`} >
-          {children}
-          { article.external && <i className="icon-budicon-519" />}
-        </a>
-      );
-    }
 
     return (
-      <NavLink href={article.url} className={`${article.external ? 'arrow-item' : ''}`}>
+      <NavLink
+        followLink={article.forceFullReload}
+        href={article.url}
+        className={`${article.external ? 'arrow-item' : ''}`}
+      >
         {children}
         { article.external && <i className="icon-budicon-519" />}
       </NavLink>
