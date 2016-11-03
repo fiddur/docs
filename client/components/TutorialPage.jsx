0965c2bd2ceb4d28831865cebcfe3f0de63c25e8
Ariel Gerstein <ariel.ger@hotmail.com>
2016-11-03 16:10:27 -0300

    Add dropdown for requirements and styles.

--- client/components/TutorialPage.jsx
+++ client/components/TutorialPage.jsx
@@ -37,16 +37,38 @@ class TutorialPage extends React.Component {
 
   componentDidMount() {
     this.initClient();
+    this.initSampleBox();
   }
 
   componentDidUpdate() {
     this.initClient();
+    this.initSampleBox();
   }
 
   initClient() {
     if (typeof document !== 'undefined') {
       this.metrics();
     }
+  }
+
+  initSampleBox() {
+    const $sampleBox = $('#package');
+    const $sampleRequirements = $sampleBox.find('.package-requirements');
+    const $requirementsToggle = $sampleBox.find('.package-requirements-toggle');
+    const $requirementsToggleText = $requirementsToggle.find('.text');
+    const $requirementsToggleIcon = $requirementsToggle.find('.icon');
+    let requirementsOpen = false;
+
+    if (!$sampleBox || !$sampleRequirements || $sampleBox.data('configured')) return;
+
+    $($requirementsToggle).on('click', () => {
+      requirementsOpen = !requirementsOpen;
+      $($sampleRequirements).slideToggle(200);
+      $($requirementsToggleText).text(`${requirementsOpen ? 'Hide' : 'Show'} requirements`);
+      $($requirementsToggleIcon).toggleClass('icon-budicon-460 icon-budicon-462');
+    });
+
+    $sampleBox.data('configured', true);
   }
 
   metrics() {
