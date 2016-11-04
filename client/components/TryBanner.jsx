2e313b0afc525db0298e59a2b753b7c23b1878c6
Alfredo Villagomez <alfredo.villagomez@auth0.com>
2016-11-04 11:58:33 -0400

    Change CTA into TryBanner

--- client/components/TryBanner.jsx
+++ client/components/TryBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 let TryBanner = (props) => (
   <div id="try-banner" data-swiftype-index='false'>
     <div className="try-banner try-banner-alt">
-      <span>Try Auth0 for FREE</span>
+      <span>Use Auth0 for FREE</span>
       <a href="javascript:signup()" className="btn btn-success btn-lg">Create free Account</a>
     </div>
   </div>
