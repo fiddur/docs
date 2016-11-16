d5fda0ccc55f3c0a8c4633e2098c6d0268bd38d2
Nate Kohari <nkohari@gmail.com>
2016-11-16 15:15:05 -0500

    Templatize snippet content in SnippetsReducer, call replaceUserVars() on API results

--- test/unit/SnippetsReducer.test.js
+++ test/unit/SnippetsReducer.test.js
@@ -7,13 +7,25 @@ import { getTestDocument, getTestFile } from '../util';
 
 describe('SnippetsReducer', () => {
 
+  // NB: The fact that this test data contains a "meta" key is a hack.
+  // In the actual system, "meta" represents the metadata of the current
+  // document. The test snippets are used both in testing the SnippetsReducer
+  // and the ReplaceIncludesPlugin, and this hack was the simplest way
+  // to avoid creating a bunch of additional test data.
+  const vars = { environment: 'test', meta: { foo: 42, bar: 123 } };
   const snippetsDir = resolve(__dirname, '../docs/snippets');
 
   describe('when the constructor is called', () => {
     describe('without a snippetsDir option', () => {
       it('throws an Error', () => {
-        const func = () => new SnippetsReducer({});
+        const func = () => new SnippetsReducer({ vars });
         expect(func).to.throw(/requires a snippetsDir option/);
+      });
+    });
+    describe('without a vars option', () => {
+      it('throws an Error', () => {
+        const func = () => new SnippetsReducer({ snippetsDir });
+        expect(func).to.throw(/requires a vars option/);
       });
     });
   });
@@ -23,7 +35,7 @@ describe('SnippetsReducer', () => {
 
     before(() => {
       const cache = new FakeCache();
-      const reducer = new SnippetsReducer({ snippetsDir });
+      const reducer = new SnippetsReducer({ snippetsDir, vars });
       result = reducer.reduce(cache);
     });
 
@@ -41,15 +53,15 @@ describe('SnippetsReducer', () => {
       expect(bar.items).to.have.length(1);
     });
 
-    it('transforms the Markdown content of each snippet', () => {
+    it('transforms the Markdown content of each snippet and replaces environmental vars', () => {
       const foo = result.get('foo/snippet-foo');
       expect(foo).to.exist;
       expect(foo.hash).to.equal('snippet-foo');
-      expect(foo.content).to.equal('<p>Snippet: foo = ${meta.foo}, bar = ${meta.bar}</p>\n');
+      expect(foo.content).to.equal('<p>Snippet: foo = 42, bar = 123</p>\n');
       const bar = result.get('bar/snippet-bar');
       expect(bar).to.exist;
       expect(bar.hash).to.equal('snippet-bar');
-      expect(bar.content).to.equal('<p>This is an example snippet with metadata.</p>\n');
+      expect(bar.content).to.equal('<p>This is an example test snippet with metadata.</p>\n');
     });
 
     it('merges metadata from snippet front matter into the entries', () => {
