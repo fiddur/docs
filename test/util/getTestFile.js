6bbb1d392ae2a7d1751b00e9d93bc6f5b17c285b
Nate Kohari <nkohari@gmail.com>
2016-11-18 15:50:53 -0500

    Ensure that all URLs in the sitemap are absolute

--- test/util/getTestFile.js
+++ test/util/getTestFile.js
@@ -3,6 +3,14 @@ import { relative, resolve } from 'path';
 import File from '../../lib/pipeline/models/File';
 
 const baseDir = resolve(__dirname, '../docs');
+
+/**
+ * Creates a File from one of the article stored in the test/docs
+ * path, for the purposes of testing.
+ * @param {String} path - The path to the article on disk, relative to test/docs.
+ * @param {String} [encoding] - The encoding to use to read the file; otherwise UTF8.
+ * @returns {File} The loaded File.
+ */
 export default function getTestFile(path, encoding = 'utf8') {
   const filename = resolve(baseDir, path);
   const shortname = relative(baseDir, filename);
