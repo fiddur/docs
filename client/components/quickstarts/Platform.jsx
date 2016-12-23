ce5608593bf9db5d50adc88cac4c92c92bcd7010
Ariel Gerstein <ariel.ger@hotmail.com>
2016-12-23 12:01:04 -0300

    Change community maintained badge icon.

--- client/components/quickstarts/Platform.jsx
+++ client/components/quickstarts/Platform.jsx
@@ -41,12 +41,10 @@ class Platform extends React.Component {
           <div className="logo">
             { get(this.props, 'platform.community') &&
               <div className="community-maintained-badge">
-                <div className="icon-container">
-                  <svg fill="#222228" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                    <path d="M0 0h24v24H0z" fill="none" />
-                  </svg>
-                </div>
+                <svg fill="#eee" height="22" viewBox="0 0 24 24" width="22" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
+                  <path d="M0 0h24v24H0z" fill="none" />
+                </svg>
               </div>
             }
           </div>
